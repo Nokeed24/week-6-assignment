ba95ee99e6f7e40faa04fefa092fa6551c215ee7
Nokeed24 <nokid_gr@hotmail.it>
2018-06-02 18:34:12 +0200

    can now add and remove toppings

--- src/components/ToppingSelection.js
+++ src/components/ToppingSelection.js
@@ -12,26 +12,26 @@ export class ToppingSelection extends PureComponent {
   
   handleChange(event) {
     let selectedTopping = event.target.value
-    let newtoppings = this.state.toppings
-    if(newtoppings.includes(selectedTopping)){
-      newtoppings.pop(selectedTopping)
-      this.setState({toppings: newtoppings})
-      this.props.selectTopping(newtoppings)
+    let statetoppings = this.state.toppings
+    if (this.state.toppings.includes(selectedTopping)){
+      //this.validateToppings(selectedTopping, true)
+      statetoppings.pop(selectedTopping)
+      this.setState({toppings: statetoppings})
     }
-    if (newtoppings.length < 3){
-      newtoppings.push(event.target.value)
-      this.setState({toppings: newtoppings})
-      this.props.selectTopping(newtoppings)
+    else{
+      //this.validateToppings(selectedTopping)
+      statetoppings.push(selectedTopping)
+      this.setState({toppings: statetoppings})
     }
-        
+    console.log(this.state.toppings)
+    this.props.selectTopping(this.state.toppings)
   }
 
   render() {
-    
     return (
       <div className="ToppingSelection">
         <h2>SELECT YOUR TOPPINGS (3 MAXIMUM)</h2>
-        <form onChange={this.handleChange}>
+        <form>
           <div className="radio">
             <label>
               <input type="radio" value="Pineapple" checked={this.state.toppings.includes("Pineapple") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
@@ -40,37 +40,37 @@ export class ToppingSelection extends PureComponent {
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Corn" checked={this.state.toppings.includes("Corn") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Corn" checked={this.state.toppings.includes("Corn")} onChange={this.handleChange}/>
               Corn
             </label>
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Olives (green)" checked={this.state.toppings.includes("Olives (green)") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Olives (green)" checked={this.state.toppings.includes("Olives (green)")} onChange={this.handleChange}/>
               Olives (green)
             </label>
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Red union" checked={this.state.toppings.includes("Red union") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Red union" checked={this.state.toppings.includes("Red union")} onChange={this.handleChange}/>
               Red union 
             </label>
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Spinach" checked={this.state.toppings.includes("Spinach") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Spinach" checked={this.state.toppings.includes("Spinach")} onChange={this.handleChange}/>
               Spinach
             </label>
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Cherry tomatoes" checked={this.state.toppings.includes("Cherry tomatoes") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Cherry tomatoes" checked={this.state.toppings.includes("Cherry tomatoes")} onChange={this.handleChange}/>
               Cherry tomatoes
             </label>
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Chicken" checked={this.state.toppings.includes("Chicken") && this.state.toppings.length <= 3} onChange={this.handleChange}/>
+              <input type="radio" value="Chicken" checked={this.state.toppings.includes("Chicken")} onChange={this.handleChange}/>
               Chicken
             </label>
           </div>
